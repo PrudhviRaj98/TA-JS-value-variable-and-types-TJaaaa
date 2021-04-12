9362e8fec461c48665f00b31df491cdb8c3f5ed7
prudhvu raj <rprudhvi645@gmail.com>
2021-04-12 09:38:32 +0530

    assignment-1

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,57 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+var user = prompt("enter a name");
+alert(`name is ${user}`);
+let userName = user;
+console.log(`${userName} ${user}`);
+let age = 22, isLearning = true;
+var user = "john";
+console.log(`user is ${user} username is ${userName}`);
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = Number(prompt("enter first number")),numB =Number(prompt("enter second number")) ;
+let sum = numA + numB;
+alert(`sum is ${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) {
+    if(j % 2 ===0){
+        console.log(j);
+    }
+}
+
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let i = 9; i>= 0 ; i--){
+    if(i % 2 !==0){
+        console.log(i);
+    }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let i = 5;
+let sum = 0;
+while ( i>0){
+    sum = sum + i;
+    console.log(sum);
+    i--;
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let i = 10;
+let sum = 0;
+while (i>0){
+    sum = sum + i ;
+    console.log(sum);
+    i--;
+}
 
 // Comparisoin
 
@@ -36,3 +71,11 @@ Example:
 "hello", -21; // false
 
 */
+let numA = Number(prompt(`enter first number`)),numB = Number(prompt(`enter second number`));
+if(String){
+    alert(`enter valid value`);
+}else{
+    alert(numA === numB);
+}
+
+
